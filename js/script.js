b8c63f5b9a22a5b3bb731e01379ddff87b55c6f7
Masum Alam <masum.7788alam@gmail.com>
2022-05-26 09:45:09 +0530

    no change

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+function eventFunction() { 
+
+    document.getElementById("Anum").innerText = 36 ;
+    document.getElementById("Lnum").innerText = 1400;
+    document.getElementById("Cpark").innerText = 560;
+    document.getElementById("Hcus").innerText = 2100;
+
+
+
+}
+
+//document.getElementById("appril").innerText = 17 + " April";
+
+
+$(document).ready(function () {
+
+
+
+})
+    
